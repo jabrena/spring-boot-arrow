90b3b29f819ba508b4575df468e8fb36768ef533
Juan Antonio Breña Moral <bren@juanantonio.info>
2020-08-16 15:56:35 +0200

    Upgrade dependencies

--- src/test/kotlin/com/jab/microservices/ApplicationTests.kt
+++ src/test/kotlin/com/jab/microservices/ApplicationTests.kt
@@ -1,10 +1,10 @@
-package com.example.demo
+package com.jab.microservices
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
